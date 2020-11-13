16168c582fb823cb20d20337175b1032afe2fb95
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2020-11-13 10:36:27 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('process.env.MONGODB_URI', {
+mongoose.connect('MONGODB_URI', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
